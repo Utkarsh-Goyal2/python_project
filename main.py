a90de2757ac2e229ccafaedd1b5b3bf0a4c09f56
Utkarsh-Goyal2 <mail2utkarshgoyal@gmail.com>
2024-11-16 16:41:00 +0530

    gherjwfe

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-print("Hello world")
-for i in range(10):
-    print(i)
+def Register():
+    users = []
+    k = True
+    class registering:
+        def __init__(self):
+            name = input("Please enter a username: ")
+            for i in users:
+                if name in i:
+                    print("Username already exists, try a different one")
+                    return 
+            passcode = input("Please enter a password:")
+            self.username = name
+            self.password = passcode
+            users.append([name,passcode])
+            global k
+            k = False
+            
+    while k:
+        user = registering()
+Register()
+      
+
